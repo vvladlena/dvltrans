1893ab5bda3e3d15162351e648b290b7b7458af5
Vladlena Tsenzeria <77752981+vvladlena@users.noreply.github.com>
2022-05-07 19:42:36 +0300

    Add files via upload

--- js/lang-list.js
+++ js/lang-list.js
@@ -1,4 +1,33 @@
 const langArr={
+
+//menu
+"menu-services":{
+    "pl": "Usługi",
+    "en": "Services",
+    "deu": "Dienstleistungen",
+},
+"menu-advantages":{
+    "pl": "Nasze korzyści",
+    "en": "Advantages",
+    "deu": "Vorteile",
+},
+"menu-cars":{
+    "pl": "Pojazdy",
+    "en": "Transport",
+    "deu": "Transport",
+},
+"menu-partners":{
+    "pl": "Partnerzy",
+    "en": "Partners",
+    "deu": "Unsere Partner",
+},
+"menu-contact":{
+    "pl": "Kontakt",
+    "en": "Contacts",
+    "deu": "Kontakte",
+},
+
+//hero
     "title-name":{
         "pl": "DVL-TRANS FIRMA TRANSPORTOWA",
         "en": "DVL-TRANS INTERNATIONAL TRANSPORT COMPANY",
@@ -9,9 +38,14 @@ const langArr={
         "en": "INTERNATIONAL TRANSPORT COMPANY",
         "deu": "INTERNATIONALES TRANSPORTUNTERNEHMEN",
     },
-    "hero-description":{
-        "pl": "Jako doświadczeni spedytorzy stawiamy na szybkość, jakość i wygodę dla klientów",
+    "hero-text":{
+        "pl": "Jesteśmy firmą transportową, która działa na terenie Unii Europejskiej od ponad 4 lat",
         "en": "Some words about company Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "deu": "Ein paar Worte zum Unternehmen Lorem ipsum dolor sit amet, consectetur",
+    },
+    "hero-description":{
+        "pl": "Jako doświadczeni spedytorzy stawiamy na szybkość, jakość i wygodę dla klientów",
+        "en": "As experienced forwarders, we focus on speed, quality and convenience for customers",
+        "deu": "Als erfahrene Spediteure konzentrieren wir uns auf Schnelligkeit, Qualität und Komfort für die Kunden",
     },
 }
