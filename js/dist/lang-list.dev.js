76de23b9169e7333e2d9cf4170fc621fad0a5858
Vladlena Tsenzeria <77752981+vvladlena@users.noreply.github.com>
2022-05-07 18:46:43 +0300

    Add files via upload

--- js/dist/lang-list.dev.js
+++ js/dist/lang-list.dev.js
@@ -4,22 +4,16 @@ var langArr = {
   "title-name": {
     "pl": "DVL-TRANS FIRMA TRANSPORTOWA",
     "en": "DVL-TRANS INTERNATIONAL TRANSPORT COMPANY",
-    "ua": "DVL-TRANS транспортна компанiя",
-    "deu": "DVL-TRANS",
-    "ru": "DVL-TRANS транспортная компания"
+    "deu": "DVL-TRANS"
   },
   "hero-title": {
     "pl": "FIRMA TRANSPORTOWA MIĘDZYNARODOWa",
     "en": "INTERNATIONAL TRANSPORT COMPANY",
-    "ua": "Мiжнародна транспортна компанiя",
-    "deu": "INTERNATIONALES TRANSPORTUNTERNEHMEN",
-    "ru": "Международная транспортная компания"
+    "deu": "INTERNATIONALES TRANSPORTUNTERNEHMEN"
   },
   "hero-description": {
-    "pl": "Jako doświadczeni spedytorzy stawiamy na SZYBKOŚĆ, JAKOŚĆ I WYGODĘ DLA KLIENTÓW",
+    "pl": "Jako doświadczeni spedytorzy stawiamy na szybkość, jakość i wygodę dla klientów",
     "en": "Some words about company Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    "ua": "Декiлька слiв про компанию Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ei",
-    "deu": "Ein paar Worte zum Unternehmen Lorem ipsum dolor sit amet, consectetur",
-    "ru": "несколько слов про компанию Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d"
+    "deu": "Ein paar Worte zum Unternehmen Lorem ipsum dolor sit amet, consectetur"
   }
 };
